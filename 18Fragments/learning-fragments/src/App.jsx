a215e6b213adbc7fcc32733eb455dcc4d2ef8220
Vaibhav Kabira <vaibhavkabira995@gmail.com>
2024-03-25 19:37:20 +0530

    updates

--- 18Fragments/learning-fragments/src/App.jsx
+++ 18Fragments/learning-fragments/src/App.jsx
@@ -1,15 +1,16 @@
 import ErrorMessage from "./components/ErrorMessage"
 import FoodItems from "./components/FoodItems"
+import Container from "./components/container"
 
 const App = () => {
 
   return (
-    <div>
+    <Container>
       <div className="display-5 text-center mx-auto p-3 mt-3">Healthy Food</div>
       <ErrorMessage />
       <FoodItems />
 
-    </div>
+    </Container>
   )
 }
 
--- 18Fragments/learning-fragments/src/components/Items.jsx
+++ 18Fragments/learning-fragments/src/components/Items.jsx
@@ -3,7 +3,7 @@ const Items = (props) => {
 
   return (
     <div>
-      <li key={props.foodItem} className={`${styles["vk-item"]} `}>
+      <li key={props.foodItem} className={`${styles["vk-item"]} list-group-item `}>
         <span className={styles['vk-span']}>{props.foodItem}</span>
       </li>
     </div>
